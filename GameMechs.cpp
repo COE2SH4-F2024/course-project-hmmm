35fb1227fe9592e468037ae41c14befc58308e57
ngy17 <ngy17@mcmaster.ca>
2024-11-17 19:27:48 -0500

    gamemechspos1

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,12 +2,23 @@
 
 GameMechs::GameMechs()
 {
-    
+    this->boardSizeX = 28; //only the size of the board that the snake can access, excludes borders
+    this->boardSizeY = 13;
+    this->score = 0;
+    this->exitFlag = false;
+    this->loseFlag = false;
+    //this->food = (random food generation code)
+
 }
 
 GameMechs::GameMechs(int boardX, int boardY)
 {
-    
+    this->boardSizeX = boardX;
+    this->boardSizeY = boardY;
+    this->score = 0;
+    this->exitFlag = false;
+    this->loseFlag = false;
+    //this->food = (random food generation code)
 }
 
 // do you need a destructor?
@@ -18,59 +29,59 @@ GameMechs::~GameMechs()
 
 bool GameMechs::getExitFlagStatus() const
 {
-
+    return this->exitFlag;
 }
 
 bool GameMechs::getLoseFlagStatus() const
 {
-
+    return this->loseFlag;
 }
     
 
 char GameMechs::getInput() const
 {
-
+    return this->input;
 }
 
 int GameMechs::getScore() const
 {
-
+    return this->score;
 }
 
 void GameMechs::incrementScore()
 {
-    
+    this->score++;
 }
 
 int GameMechs::getBoardSizeX() const
 {
-
+    return this->boardSizeX;
 }
 
 int GameMechs::getBoardSizeY() const
 {
-
+    return this->boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    this->exitFlag = true;
 }
 
 void GameMechs::setLoseFlag()
 {
-    
+    this->loseFlag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    this->input = this_input;
 }
 
 void GameMechs::clearInput()
 {
-
+    this->input = '\0';
 }
 
 // More methods should be added here
