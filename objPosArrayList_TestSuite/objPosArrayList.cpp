3d8e7b2daa209856e45e410b6df6ebb1b6e4ff46
obljubej <obljubej@mcmaster.ca>
2024-11-25 16:18:54 -0500

    Added arraylist functionality inside of TestSuite

--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,50 +5,71 @@
 
 objPosArrayList::objPosArrayList()
 {
-    
+    listSize = 0;
+    arrayCapacity = 200;
+    aList = new objPos[arrayCapacity];
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList;
+    aList = nullptr;
+    listSize = 0;
+    arrayCapacity = 0;
 }
 
 int objPosArrayList::getSize() const
 {
-
+    return listSize;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
-    
+    if (listSize < arrayCapacity) {
+        //Shift all elements to the right
+        for (int i = listSize; i > 0; i--) {
+            aList[i] = aList[i - 1];
+        }
+        aList[0] = thisPos;
+        listSize++;
+    }
 }
 
 void objPosArrayList::insertTail(objPos thisPos)
 {
-    
+    if (listSize < arrayCapacity) {
+        aList[listSize] = thisPos;
+        listSize++;
+    }
 }
 
 void objPosArrayList::removeHead()
 {
-    
+    if (listSize > 0) {
+        for (int i = 0; i < listSize - 1; i++) {
+            aList[i] = aList[i + 1];
+        }
+        listSize--;
+    }
 }
 
 void objPosArrayList::removeTail()
 {
-    
+    aList[listSize - 1] = objPos();
+    listSize--;
 }
 
 objPos objPosArrayList::getHeadElement() const
 {
-    
+    return aList[0];
 }
 
 objPos objPosArrayList::getTailElement() const
 {
-    
+    return aList[listSize - 1];
 }
 
 objPos objPosArrayList::getElement(int index) const
 {
-    
+    return aList[index];
 }
